51c5f6165c486196c42923634ba7bd82c7a52bda
Francis7575 <francis.albert.2009@gmail.com>
2024-11-09 23:19:23 -0800

    installed @google/generative/ai, get gemini_api_key and display the prompt in the console

--- src/app/dashboard/create-new/page.tsx
+++ src/app/dashboard/create-new/page.tsx
@@ -1,12 +1,18 @@
 'use client'
-import { useState } from "react"
+import { MouseEvent, useState } from "react"
 import SelectTopic from "./_components/SelectTopic"
 import SelectStyle from "./_components/SelectStyle"
 import SelectDuration from "./_components/SelectDuration"
 import { Button } from "@/components/ui/button"
 
+type formDataProps = {
+  topic?: string
+  imageStyle?: string
+  duration?: string
+}
+
 const CreateNew = () => {
-  const [formData, setFormData] = useState([])
+  const [formData, setFormData] = useState<formDataProps>({})
 
   const handleInputChange = (fieldName: string, fieldValue: string) => {
     console.log(fieldName, fieldValue);
@@ -17,15 +23,43 @@ const CreateNew = () => {
     }))
   }
 
+  const createClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    console.log("Button clicked, starting getVideoScript...");
+    getVideoScript()
+  }
+
+  const getVideoScript = async () => {
+    const prompt =  'Write a script to generate ' + formData.duration + ' video on topic : ' + formData.topic + ' along with AI image prompt in ' + formData.imageStyle + ' format for each scene and give me result in JSON format with imagePrompt and ContextText as field, No Plain text'
+    console.log(prompt);
+    
+    // try {
+    //   const response = await fetch('api/get-video-script', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //       // prompt: 
+    //     })
+    //   })
+    // } catch (e) {
+
+    // }
+  }
+
   return (
     <div className="md:px-20">
       <h2 className="font-bold text-4xl text-primary text-center">Create New</h2>
       <div className="mt-10 shadow-md p-10">
-        <SelectTopic onUserSelect={handleInputChange}/>
+        <SelectTopic onUserSelect={handleInputChange} />
         <SelectStyle onUserSelect={handleInputChange} />
         <SelectDuration onUserSelect={handleInputChange} />
 
-        <Button className="mt-10 w-full">Create Short Video</Button>
+        <Button onClick={createClickHandler}
+          className="mt-10 w-full">
+          Create Short Video
+        </Button>
       </div>
     </div>
   )
