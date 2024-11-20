7b3b177537823b8ff70d8b56f8727510e1d4fc66
Francis7575 <francis.albert.2009@gmail.com>
2024-11-20 14:29:22 -0800

    Succesfully added captions to the video

--- src/app/dashboard/_components/RemotionVideo.tsx
+++ src/app/dashboard/_components/RemotionVideo.tsx
@@ -1,12 +1,13 @@
 'use client'
 import { RemotionVideoProps } from "@/types/types"
 import { useEffect } from "react"
-import { AbsoluteFill, Audio, Img, Sequence, useVideoConfig } from "remotion"
+import { AbsoluteFill, Audio, Img, Sequence, useCurrentFrame, useVideoConfig } from "remotion"
 
 
 const RemotionVideo = ({ script, imageList, audioFileUrl, captions, setDurationInFrame }: RemotionVideoProps) => {
 
   const { fps } = useVideoConfig()
+  const frame = useCurrentFrame();
 
   useEffect(() => {
     if (captions && captions.length > 0) {
@@ -20,6 +21,12 @@ const RemotionVideo = ({ script, imageList, audioFileUrl, captions, setDurationI
     ? (captions[captions.length - 1].end / 1000) * fps
     : 0;
 
+  const getCurrentCaptions = () => {
+    const currentTime = frame / 30 * 1000 //Convert frame number to milliSeconds (30fps)
+    const currentCaption = captions?.find((w) => currentTime >= w.start && currentTime <= w.end)
+    return currentCaption ? currentCaption?.text : '';
+  }
+
   return (
     <AbsoluteFill className='bg-black'>
       {imageList?.map((item, idx) => {
@@ -27,14 +34,30 @@ const RemotionVideo = ({ script, imageList, audioFileUrl, captions, setDurationI
 
         return (
           <Sequence key={idx} from={startFrame} durationInFrames={durationFrames}>
-            <Img
-              src={item}
-              style={{
-                width: '100%',
-                height: '100%',
-                objectFit: 'cover'
-              }}
-            />
+            <AbsoluteFill>
+              <Img
+                src={item}
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover'
+                }}
+              />
+              <AbsoluteFill style={{
+                color: 'white',
+                justifyContent: 'center',
+                top: undefined,
+                bottom: 50,
+                height: 150,
+                textAlign: 'center',
+                width: '100%'
+              }}>
+                <h2 className="text-2xl">
+                  {getCurrentCaptions()}
+                </h2>
+              </AbsoluteFill>
+            </AbsoluteFill>
+
           </Sequence>
         )
       })}
